bfaa599a23f4fb06466a80e4d71af5dcb251bd7e
Ravin De Silva <ravin.de.silva@stud.fh-campuswien.ac.at>
2022-03-20 16:57:22 +0100

    AppController classes

--- src/main/java/ac/at/fhcampuswien/AppController.java
+++ src/main/java/ac/at/fhcampuswien/AppController.java
@@ -1,27 +1,33 @@
 package ac.at.fhcampuswien;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class AppController {
 
-    ArrayList<String> articles = new ArrayList<String>();
+    private ArrayList<String> articles = new ArrayList<String>();
 
 
-    public void AppConroller() {
-
-
-
+    public AppController() {
     }
 
-    public void setArticles() {
-
-
+    public void setArticles(List<Article> articles) {
     }
 
     public int getArticleCount() {
-
-
         return 0;
+    }
+
+    public List<Article> getTopHeadlinesAustria() {
+    }
+
+    public List<Article> getAllNewsBitcoin() {
+    }
+
+    protected static List<Article> filterList(String query, List<Article> articles) {
+    }
+
+    private static List<Article> generateMockList() {
     }
 
 
