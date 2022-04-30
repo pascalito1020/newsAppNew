2df85df19e1f7cf568ed733c6e3b765e8d6c80d9
Christian17f <christian.fichtenbauer@stud.fh-campuswien.ac.at>
2022-04-30 17:33:12 +0200

    implemented NewsResponse

---
+++ src/main/java/ac/at/fhcampuswien/NewsResponse.java
@@ -0,0 +1,23 @@
+package ac.at.fhcampuswien;
+
+import java.util.ArrayList;
+
+public class NewsResponse {
+    String status;
+    int totalResults;
+
+    public ArrayList<Article> articles = new ArrayList<>();
+
+
+    public String getStatus() {
+        return status;
+    }
+
+    public int getTotalResults() {
+        return totalResults;
+    }
+
+    public ArrayList<Article> getArticles() {
+        return articles;
+    }
+}
