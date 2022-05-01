c9345b2fbb3d22f51a9c18d365b1323e7094e182
jayone2x <Ssbbkkpp10>
2022-05-01 23:39:16 +0200

    Override

--- src/main/java/ac/at/fhcampuswien/Article.java
+++ src/main/java/ac/at/fhcampuswien/Article.java
@@ -4,12 +4,27 @@ public class Article {
 
     private String author;
     private String title;
-
+//    private  String description;
+//    private  String url;
+//    private  String urlToImage;
+//    private  String publishedAt;
+//    private  String content;
+//    private  String SubClassIdAndName;
 
     public Article(String author, String title) {
         this.author = author;
         this.title = title;
     }
+
+//    public Article(String author, String title, String description, String url, String urlToImage, String publishedAt, String content) {
+//        this.author = author;
+//        this.title = title;
+//        this.description = description;
+//        this.url = url;
+//        this.urlToImage = urlToImage;
+//        this.publishedAt = publishedAt;
+//        this.content = content;
+//    }
 
     public String getAuthor() {
         return author;
@@ -21,9 +36,13 @@ public class Article {
 
     @Override
     public String toString() {
-        return "Article{" +
-                "author='" + author + '\'' +
-                ", title='" + title + '\'' +
-                '}';
+        return "\nArticle \n" +
+                "Author: " + author + "\n" +
+                "Title: " + title + "\n";
+//                "Description: " + description + "\n" +
+//                "URL: " + url + "\n" +
+//                "Image Link: " + urlToImage + "\n" +
+//                "published at: " + publishedAt + "\n" +
+//                "Content: " + content + "\n";
     }
 }
